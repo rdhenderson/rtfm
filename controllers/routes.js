98882bdf664a287d2a8806c7b41e4f598b23066e
vcore83 <Saggy@MacBook-Pro.local>
2017-05-20 13:38:16 -0400

    basic-framework

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,27 +1,37 @@
 //Require sequelize models
-var db = require("../models");
+//var db = require("../models");
 var path = require('path');
 
 module.exports = function(app) {
 
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/test.html"));
+    // var queryURL = "https://api.stackexchange.com/2.2/search?" +  "order=desc&sort=activity&site=stackoverflow";
+    //
+    // $.ajax({
+    //   url: queryURL,
+    //   method: "GET"
+    // }).done(function(res){
+    //
+    //   console.log(res);
+    // })
+
+    res.end("It worked!");
   });
 
-  app.get('/api/search', function (req, res) {
-      // db.Burger.findAll().then((results) => res.json(results))
-  });
-
-  app.post('/api/burger', function(req, res) {
-    // db.Burger.create(req.body).then((dbPost) => res.json(dbPost));
-  });
-
-  app.put('/api/burger', function(req, res) {
-    // db.Burger.update(req.body, {where : { id : req.body.id}})
-    // .then((dbPost) => res.json(dbPost));
-  });
-
-  app.delete('/api/burger/:id', function(req,res) {
-    // db.Burger.destroy({where : {id: req.params.id}}).then((results) => res.json(results));
-  });
+  // app.get('/api/search', function (req, res) {
+  //     // db.Burger.findAll().then((results) => res.json(results))
+  // });
+  //
+  // app.post('/api/burger', function(req, res) {
+  //   // db.Burger.create(req.body).then((dbPost) => res.json(dbPost));
+  // });
+  //
+  // app.put('/api/burger', function(req, res) {
+  //   // db.Burger.update(req.body, {where : { id : req.body.id}})
+  //   // .then((dbPost) => res.json(dbPost));
+  // });
+  //
+  // app.delete('/api/burger/:id', function(req,res) {
+  //   // db.Burger.destroy({where : {id: req.params.id}}).then((results) => res.json(results));
+  // });
 };
