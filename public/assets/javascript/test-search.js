64fdd11d979bc9420c97db3023f9d6e73ba4a130
Reid Henderson <reid.henderson@gmail.com>
2017-05-25 11:25:04 -0400

    WIP: working on good formatting for displaying data in two divs

--- public/assets/javascript/test-search.js
+++ public/assets/javascript/test-search.js
@@ -1,65 +1,49 @@
 $(document).ready( () => {
   $('#search-submit').on('click', searchHandler);
-  // $('#search-term').on('keyup', keyHandler);
 
-  //Init Methods and then populate fuzzy search handler
+  // Init Methods and then populate fuzzy search handler
   getMethods( (methods) => {
-    console.log('methods ', methods);
-    //Remove argument strings from autocomplete (consider removing from search submission instead)
+    // Remove argument strings from autocomplete (consider removing from search submission instead)
     const methodArray = methods.map( (el) => {
       return {
         shortName: el.name.split('(')[0] //,
-        // name: el.name,
-        // html: el.html
       }
     });
 
-
-    // $( "#search-term" ).autocomplete({ source: availableTags });
-    // console.log('availableTags :', availableTags);
-
-    // console.log('method list', methodListItems);
-    $('#search-express').fuzzyComplete(methodArray);
+    $('#search-input').fuzzyComplete(methodArray);
     $('input').on('keyup blur', () => {
-      // console.log('Sensing keys');
       $(this).parent().find(".output").html($(this).parent().find("select").val());
     });
 
-    //Print list of methods
+    // Print list of methods
     const methodListItems = methods.reduce( (string, elem) => string += '<li>' + elem.name + '</li>', '<ul>') + '</ul>';
-    $('#methods-list').append(methodListItems);
+    $('#stack-div').append(methodListItems);
   });
-
-
-
 });
 
 function getMethods(callback) {
   $.get('/api/express/methods', (data) => {
-    // console.log('methods', resp);
     callback(data)
   });
 }
-//
-// function keyHandler(e) {
-//   switch (e.keyCode) {
-//     case 13: //enter key, simulate a click
-//       e.preventDefault();
-//       $('#search-term').click();
-//       break;
-//     default:
-//       console.log(e.key);
-//       break;
-//  }
-// }
 
 function searchHandler() {
-    // hideImagesOnSearch();
+    hideImages();
     //Strip the arguments portion of name before query
-    let query = encodeURIComponent($('#search-express').val().split('(')[0].trim());
+    let query = encodeURIComponent($('#search-input').val().split('(')[0].trim());
     //Template literal expansion using backticks instead of quote/apostrophe
     $.get(`/api/express/search/${query}`, (resp) => {
       console.log('html', resp.html);
-      $('#docs-div').empty().html(resp.html);
+      $('#documentation-div').empty().html(resp.html);
     });
+}
+
+//Look into a simpler .toggle
+function hideImages() {
+  $("#stack-div").show();
+  $("#documentation-div").show();
+  $("#stack-div").removeClass("hidden");
+  $("#documentation-div").removeClass("hidden");
+  $("#stack-div-show").addClass("hidden");
+  $("#documentation-show").addClass("hidden");
 }
