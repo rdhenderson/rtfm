fc0d9efa2a8eeb85a54a97640bf1bc4da5fc18ed
Reid Henderson <reid.henderson@gmail.com>
2017-06-02 20:46:38 -0400

    Updated jquery parsing and flattened jquery model.  Improved accessibility of jquery information.

---
+++ db/parseJqueryFiles.js
@@ -0,0 +1,75 @@
+const searchJquery = require('../search_modules/search-jquery.js');
+const updateJqueryDetails = require('../search_modules/jquery-detail-update.js');
+const cheerio = require('cheerio');
+
+const fs = require('fs');
+const db = require('../models');
+
+//Create empty methods ARRAY
+let methodsList = [];
+
+// PARSE JQUERY API LIST TO CREATE METHOD ARRAY
+function genMethodList(){
+  const body = fs.readFileSync(__dirname + '/tmp/jquery-api.html');
+  const $ = cheerio.load(body);
+  //Add each section within api-doc as object to methods
+  $(".entry-title").each( (i, el) => {
+    const data_url = $(el).children('a:first-child').attr('href').split('/')[3];
+    methodsList.push(data_url);
+  });
+  return methodsList;
+}
+
+//Get stored jquery file and pass text to next function
+function getMethodFile(fileName){
+  return new Promise( (resolve, reject) => {
+    fs.readFile(__dirname + `/tmp/jquery/${fileName}.html`, (err, data) => {
+          if (err) reject(err);
+          resolve(data);
+        });
+    });
+}
+
+function parseMethodDetail(body) {
+    // Load method html file as body for jquery-style parsing
+  return new Promise( (resolve, reject) => {
+    const $ = cheerio.load(body);
+    const name = $(".entry-title").html();
+    // const data_url = item;
+    const desc = $(".desc").html();
+    const detail = $("#entry-longdesc").html();
+    // const signatures =  $(".signature").map( (i, e) => $(e).html()).get();
+    const examples = $(".entry-example").map( (i,e) => $(e).html()).get();
+    // for (let key in examples) console.log(key + " : " + examples[key]);
+    // console.log("Examples", examples);
+    const newMethod = {
+      name : name,
+      // data_url : data_url,
+      description : desc,
+      detail : detail,
+      examples : examples
+    };
+    // Send new method on to next function in chain
+    resolve(newMethod);
+  });
+}
+
+db.sequelize.sync({ force: true }).then( () => {
+
+  // Update express database
+  require('../search_modules/search-express.js').updateDB( () => console.log('Updated Express Table'));
+
+  let methodsList = genMethodList();
+  methodsList.forEach( (data_url) => {
+    getMethodFile(data_url)
+      .then( parseMethodDetail )
+      .then( (method) => {
+        method.data_url = data_url; //Need to add data_url from api-list
+        console.log('Adding name: ', data_url);
+        return db.JQueryDoc.create(method)
+    }).then( () => {
+      if (methodsList.indexOf(data_url) === methodsList.length-1)
+        console.log(`Updated JQuery Table in Database`)
+    }).catch( (err) => console.log('ERROR: ', err));
+  });
+});
